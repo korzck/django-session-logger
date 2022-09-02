ac99b5db0c83fe316efd4eddbde165262e46f97a
korzck <konstantin.koretskiy@gmail.com>
2022-09-02 21:36:44 +0300

    Added compact output

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # django-session-logger
-This django app allows to preview connections that have been made to the app
-<br>
-The app is currently in development and thus has enabled debug mode. For deployment you need to manage your own setting.py.
+This django app allows to list connections that have been made to the app.
+<br />
+The app is currently in development and thus has debug mode, revealed secret key and more. For deployment you need to manage with your own configuration.
--- logger/views.py
+++ logger/views.py
@@ -1,15 +1,23 @@
 from copy import copy
+from typing import Dict
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     headers = copy(request.headers)
-    for i in headers:
-        i = str(i).replace('-', '_')
+    # for i in headers:
+    #     i = str(i).replace('-', '_')
     ip = get_client_ip(request)
     meta = request.META
+    
+    fields = {}
+    fields['IP'] = ip
+    fields['Shell'] = meta['SHELL']
+    fields['User agent'] = headers['User-Agent']
+    fields['Accept language'] = headers['Accept-Language']
+    fields['Country'] = ''
 
-    return render(request, 'index.html', context={'headers': headers, 'meta':meta, 'ip':ip})
+    return render(request, 'index.html', context={'fields':fields})
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
